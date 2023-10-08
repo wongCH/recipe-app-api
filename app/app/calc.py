7557d59c0eb9cf7d654c4e686cdb8c6dd1bd54fe
wong_hun@gmail.com <wong_.hun@gmail.com>
2023-10-08 19:20:53 +0800

    add db service

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Add x n y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+
+    return y - x
+
